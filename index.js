3f0c2950eeedbf4284a8230dad2c8e77acb3e360
Christian Bundy <christianbundy@fraction.io>
2020-02-18 10:41:11 -0800

    Fix ESLint not linting other files
    Problem: Unlike `tsc`, the `eslint` binary only looks at the exact files
    that you pass to it, so just passing the entry file doesn't actually
    lint the other files.
    
    Solution: Ensure that `eslint` is linting all of the JS and TS files in
    the repo.

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ if (main === "") {
 const eslintConfig = require.resolve("./.eslintrc");
 
 const checkCommands = [
-  `eslint -c ${eslintConfig} ${main}`,
+  `eslint -c ${eslintConfig} **/*.{js,ts}`,
   "stylelint --allow-empty-input **/*.css",
   `tsc --allowJs --resolveJsonModule --lib es2018,dom --checkJs --noEmit --skipLibCheck ${main}`,
   "dependency-check --missing -i common-good ./package.json",
@@ -38,7 +38,7 @@ const checkCommands = [
 const fixCommands = [
   "prettier --write **/*.{js,ts,jsx,json,css,scss,less,html,vue,gql,md,yaml}",
   "stylelint --fix --allow-empty-input **/*.css",
-  `eslint -c ${eslintConfig} --fix ${main}`
+  `eslint -c ${eslintConfig} --fix **/*.{js,ts}`
 ];
 
 const run = command => {
